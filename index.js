2f3dc6a78bca5cbbb4509c7cad4b3f5a66a6a118
sakthi1307 <sakthi.enk@gmail.com>
2021-11-11 10:07:10 +0530

    changed Client config

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-const { Player } = require('discord-player');
-const { Client, Intents } = require('discord.js');
+const { Player } = require("discord-player");
+const { Client, Intents } = require("discord.js");
 
 global.client = new Client({
-    intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MEMBERS,
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_VOICE_STATES
-    ],
-    disableMentions: 'everyone',
+  intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "GUILD_VOICE_STATES"],
+  disableMentions: "everyone",
+  partials: ["CHANNEL"],
 });
 
-client.config = require('./config');
+client.config = require("./config");
 
 global.player = new Player(client, client.config.opt.discordPlayer);
 
-require('./src/loader');
-require('./src/events');
+require("./src/loader");
+require("./src/events");
 
 client.login(client.config.app.token);
